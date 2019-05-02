325b4a0bda3f3bc88b87d87531826fab4ffd3233
worud1205 <jinjk.jgs@gmail.com>
2019-05-02 14:43:28 +0900

    my first commit

--- app.js
+++ app.js
@@ -2,13 +2,31 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-/*app.get('/',function (req,res) {
-  res.send('Hello World!');
+app.set("view engine", 'ejs');
+app.use(express.static(path.join(__dirname + 'public')));
+//console.log(__dirname);
+
+var data={count:0};
+app.get('/', function (req,res) {
+  data.count++;
+  res.render('my_first_ejs', data);
 });
-*/
-app.use(express.static(path.join(__dirname + '/public')));
-console.log(__dirname);
+
+app.get('/reset', function (req,res) {
+  data.count=0;
+  res.render('my_first_ejs', data);
+});
+
+app.get('/set/count', function (req,res) {
+  if(req.query.count) data.count=req.query.count;
+  res.render('my_first_ejs', data);
+});
+
+app.get('/set/:num', function (req,res) {
+  data.count=req.params.num;
+  res.render('my_first_ejs', data);
+});
 
 app.listen(3000, function(){
   console.log('Server On!');
-});
+})
