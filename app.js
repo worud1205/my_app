b053cb25d5d6feb3f07e78f3b6dc4c552bf792af
worud1205 <jinjk.jgs@gmail.com>
2019-05-02 12:39:36 +0900

    my first commit

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.get(
-  '/',function (req,res)
-  {
-    res.send('Hello World!');
-  }
-);
+/*app.get('/',function (req,res) {
+  res.send('Hello World!');
+});
+*/
+app.use(express.static(path.join(__dirname + '/public')));
+console.log(__dirname);
 
-app.listen(
-  3000, function()
-  {
-    console.log('Server On!');
-  }
-);
+app.listen(3000, function(){
+  console.log('Server On!');
+});
