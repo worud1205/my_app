e40948fac89f96bae9a471e4159f49e4a309fd18
worud1205 <jinjk.jgs@gmail.com>
2019-05-03 09:14:38 +0900

    my first git

--- app.js
+++ app.js
@@ -1,10 +1,57 @@
+
+//console.log(__dirname);
+///*/*----------------------------------------------------------------------------------------------*///npm install tedious 후 복붙va
+var sql = require('mssql');
+var Connection = require('tedious').Connection;     //tedious폴더 mssql로 이름 변경
+var Request = require('tedious').Request;
+
+
+
+var config = {
+    server: '192.168.0.97',
+    options: { encrypt:false, database: 'Test_MJ' },
+    authentication:{
+        type:"default",
+        options:{
+            userName:"MJ",
+            password:"1234"
+        }
+    }
+};
+
+
+var connection = new Connection(config);
+connection.on('connect', function(err) {
+    console.log("Connected");
+    executeStatement();
+});
+
+function executeStatement(){
+request = new Request("select Convert(char(10),getdate(),121) as TargetDate, newid()", function(err, rowCount) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(rowCount + ' rows');
+        }
+    });
+
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log(column.value);
+        });
+    });
+
+    connection.execSql(request);
+}
+
+
+/*----------------------------------------------------------------------------------------------*/
 var express = require('express');
 var path = require('path');
 var app = express();
 
 app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname + 'public')));
-//console.log(__dirname);
 
 var data={count:0};
 app.get('/', function (req,res) {
